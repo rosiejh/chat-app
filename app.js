4999607b8dde501d2dd216b02c63b848fb6722c7
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-27 16:30:03 +1100

    Initial commit 🚀

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.render(index.html)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server has started!')
+})
