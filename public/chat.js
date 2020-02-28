c9e4922ff841c0bcf8045e58bb550932fcbba898
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-28 16:20:38 +1100

    [update] Sharing location

--- public/chat.js
+++ public/chat.js
@@ -7,7 +7,20 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const message = document.querySelector('input').value
+    const message = e.target.elements.message.value
     
     socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
 })
